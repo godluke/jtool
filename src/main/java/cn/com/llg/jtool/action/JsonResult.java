571ea7df3a4540f167e72666d5fabccd09b7468e
godluke <840566281@qq.com>
2016-02-26 19:10:16 +0800

    Update JsonResult.java

--- src/main/java/cn/com/llg/jtool/action/JsonResult.java
+++ src/main/java/cn/com/llg/jtool/action/JsonResult.java
@@ -45,7 +45,97 @@ public class JsonResult {
         this.e = e ;
         this.fialMsg = e.getMessage() ;
     }
-
+    HttpServletRequest request ;
+    HttpServletResponse response ;
+    
+    /**
+	 * @param json java JSONObject对象
+	 * @param response HttpServletResponse
+	 */
+	public void responseWriteJson() {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/Json");
+//		response.setContentType("text/html");
+		try {
+			// response.getWriter().print(o);
+			PrintWriter pw = response.getWriter();
+			pw.print(this);
+			pw.flush();
+			pw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		alog.info("====================返回数据=====================");
+	}
+    
+    /**
+	 * 请求正常返回数据
+	 * 
+	 * @param response  HttpServletResponse
+	 * @param jsonResult JsonResult json返回对象
+	 * @param u  User
+	 * @param jsonCfg json去除属性
+	 */
+	protected void actionOver(User u, String... jsonCfg) {
+		this.setUrl(request.getRequestURI());
+		Map<String, String[]> param = request.getParameterMap() ;
+		this.setJsonParam(JSONObject.fromObject(param).toString());
+		alog.info(this.getJsonParam());
+		if(this.getData()==null){
+			Map<String, String> m = new HashMap<String, String>(1) ;
+			m.put("success", "ok") ;
+			this.setData(m);
+		}
+		Date date = new Date();
+		this.setEnd(date);
+		if(this.getData()==null){
+			this.setData(new HashMap<String,String>().put("actionOver", "OK"));
+		}
+		if (u != null) {
+			this.setWorkerId(u.getId());
+			this.setCompanyId(u.getCompanyId());
+			this.setStoreId(u.getStoreId());
+			this.setDepId(u.getDepId());
+		}
+		JSONObject jo;
+		if (jsonCfg != null && jsonCfg.length > 0) {
+			JsonConfig jc = new JsonConfig();
+			jc.setExcludes(jsonCfg);
+			jo = JSONObject.fromObject(jsonResult, jc);
+		} else {
+			jo = JSONObject.fromObject(jsonResult);
+		}
+		
+		
+		responseWriteJson(jo, response);
+		
+	}
+	
+	
+	
+	/**
+	 * 请求异常返回数据
+	 * 
+	 * @param response  HttpServletResponse
+	 * @param e  Exception
+	 * @param jsonCfg  json去除属性
+	 */
+	public void actionExceptionOver(Exception e, String... jsonCfg) {
+	    /*
+		e.printStackTrace();
+		JsonResult jsonResult = JsonResult.generate("Exception");
+		jsonResult.setExceptionMessage( e.getMessage());
+		jsonResult.setExceptionClass(e.getClass().toString());
+		jsonResult.setData(e);
+		jsonResult.setSuccess(JsonResult.Fail);
+		
+		Map<String, String[]> param = request.getParameterMap() ;
+		jsonResult.setJsonParam(JSONObject.fromObject(param).toString());
+        */
+		actionOver(request,response, jsonResult, getCurrentUser(request), jsonCfg);
+	}
+    
+    
     String fialMsg ;
     Exception e ;
     Long startTime ;
@@ -146,4 +236,9 @@ public class JsonResult {
     public void setE(Exception e) {
         this.e = e;
     }
+    
+    
+    
+    
+    
 }
