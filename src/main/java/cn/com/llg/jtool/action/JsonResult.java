e8e5fc42a6583b3693e00f52ea97b8c3b4ff441a
godluke <llg6yy@163.com>
2016-03-01 12:06:17 +0800

    修改JsonResult类，个性返回json数据方法

--- src/main/java/cn/com/llg/jtool/action/JsonResult.java
+++ src/main/java/cn/com/llg/jtool/action/JsonResult.java
@@ -1,11 +1,17 @@
 package cn.com.llg.jtool.action;
 
-import org.apache.log4j.Logger;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
-import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
 
 /**
  * 请求返回数据通用格式
@@ -13,6 +19,36 @@ import java.util.Map;
  */
 public class JsonResult {
     private static Logger log = Logger.getLogger(JsonResult.class) ;
+    
+    
+    HttpServletRequest request ;
+    HttpServletResponse response ;
+    
+    /**异常信息*/
+    String fialMsg ;
+    /**异常*/
+    Exception e ;
+    /**请求开始时间*/
+    Long startTime ;
+    /**请求结束时间*/
+    Long endTime ;
+    /**请求地址是做事件描述*/
+    String working ;
+    /**请求参数*/
+    Map<String, String[]> param ;
+    /**请求地址*/
+    String url ;
+    /**请求反回结果*/
+    JsonResultOverType overType ;
+    /**返回数据*/
+    Object data ;
+    /**操作人姓名*/
+    String userName ;
+    /**操作人id*/
+    String userId ;
+    /**操作人在哪里操作的*/
+    String userLocation ;
+    
 
     public enum JsonResultOverType{
         success,
@@ -21,43 +57,62 @@ public class JsonResult {
 
     private JsonResult(){} ;
 
-    public static JsonResult create(HttpServletRequest request,HttpServletResponse response,String working,String userName,String userLocation){
+    public static JsonResult create(HttpServletRequest request,HttpServletResponse response,String working,String userName,String userId,String userLocation){
         log.debug("================JsonResult create================ ");
         JsonResult jr = new JsonResult() ;
         jr.startTime = new Date().getTime() ;
         jr.working = working ;
-        jr.overType = JsonResultOverType.success ;
         jr.url = request.getRequestURI() ;
         jr.param = request.getParameterMap() ;
         jr.userName = userName ;
         jr.userLocation = userLocation ;
+        jr.userId = userId ;
         return jr ;
     }
 
-    public void setSuccess(Object Data){
+	private void setSuccess(Object data){
+    	log.debug("================JsonResult success================ ");
+    	this.overType = JsonResultOverType.success ;
         this.endTime = new Date().getTime() ;
         this.data = data ;
     }
-
-    public void setFail(Exception e){
-        log.info(e.getMessage());
+    /**请求成功*/
+    @SuppressWarnings("unused")
+	public void requestSuccess(Object data,String... jsonCfg){
+    	this.setSuccess(data);
+    	JSONObject jo = null ;
+		if (jsonCfg != null && jsonCfg.length > 0) {
+			JsonConfig jc = new JsonConfig();
+			jc.setExcludes(jsonCfg);
+			jo = JSONObject.fromObject(this, jc);
+		} else {
+			jo = JSONObject.fromObject(this);
+		}
+		this.responseWriteJson();
+    }
+    private void setFail(Exception e){
+    	log.debug("================JsonResult fail================ ");
+    	e.printStackTrace();
+        this.overType = JsonResultOverType.fail ;
         this.endTime = new Date().getTime() ;
         this.e = e ;
         this.fialMsg = e.getMessage() ;
     }
-    HttpServletRequest request ;
-    HttpServletResponse response ;
     
-    /**
-	 * @param json java JSONObject对象
-	 * @param response HttpServletResponse
-	 */
+    /**请求失败*/
+    public void requestSuccess(Exception e){
+    	this.setFail(e);
+		this.responseWriteJson();
+    }
+    
+   
+    
+    /**写设置好的信息*/
 	public void responseWriteJson() {
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("application/Json");
 //		response.setContentType("text/html");
 		try {
-			// response.getWriter().print(o);
 			PrintWriter pw = response.getWriter();
 			pw.print(this);
 			pw.flush();
@@ -65,93 +120,26 @@ public class JsonResult {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		alog.info("====================返回数据=====================");
 	}
-    
-    /**
-	 * 请求正常返回数据
-	 * 
-	 * @param response  HttpServletResponse
-	 * @param jsonResult JsonResult json返回对象
-	 * @param u  User
-	 * @param jsonCfg json去除属性
-	 */
-	protected void actionOver(User u, String... jsonCfg) {
-		this.setUrl(request.getRequestURI());
-		Map<String, String[]> param = request.getParameterMap() ;
-		this.setJsonParam(JSONObject.fromObject(param).toString());
-		alog.info(this.getJsonParam());
-		if(this.getData()==null){
-			Map<String, String> m = new HashMap<String, String>(1) ;
-			m.put("success", "ok") ;
-			this.setData(m);
-		}
-		Date date = new Date();
-		this.setEnd(date);
-		if(this.getData()==null){
-			this.setData(new HashMap<String,String>().put("actionOver", "OK"));
-		}
-		if (u != null) {
-			this.setWorkerId(u.getId());
-			this.setCompanyId(u.getCompanyId());
-			this.setStoreId(u.getStoreId());
-			this.setDepId(u.getDepId());
-		}
-		JSONObject jo;
-		if (jsonCfg != null && jsonCfg.length > 0) {
-			JsonConfig jc = new JsonConfig();
-			jc.setExcludes(jsonCfg);
-			jo = JSONObject.fromObject(jsonResult, jc);
-		} else {
-			jo = JSONObject.fromObject(jsonResult);
-		}
-		
-		
-		responseWriteJson(jo, response);
-		
-	}
-	
-	
 	
 	/**
-	 * 请求异常返回数据
-	 * 
-	 * @param response  HttpServletResponse
-	 * @param e  Exception
-	 * @param jsonCfg  json去除属性
+	 * 写回json信息
+	 * @param json
 	 */
-	public void actionExceptionOver(Exception e, String... jsonCfg) {
-	    /*
-		e.printStackTrace();
-		JsonResult jsonResult = JsonResult.generate("Exception");
-		jsonResult.setExceptionMessage( e.getMessage());
-		jsonResult.setExceptionClass(e.getClass().toString());
-		jsonResult.setData(e);
-		jsonResult.setSuccess(JsonResult.Fail);
-		
-		Map<String, String[]> param = request.getParameterMap() ;
-		jsonResult.setJsonParam(JSONObject.fromObject(param).toString());
-        */
-		actionOver(request,response, jsonResult, getCurrentUser(request), jsonCfg);
+	public void responseWriteJson(JSONObject json) {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/Json");
+//		response.setContentType("text/html");
+		try {
+			PrintWriter pw = response.getWriter();
+			pw.print(json);
+			pw.flush();
+			pw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
     
-    
-    String fialMsg ;
-    Exception e ;
-    Long startTime ;
-    Long endTime ;
-    String working ;
-    Map<String, String[]> param ;
-    String url ;
-    JsonResultOverType overType ;
-    /**返回数据*/
-    Object data ;
-    /**操作人姓名*/
-    String userName ;
-    /**操作人在哪里操作的*/
-    String userLocation ;
-    HttpServletRequest request ;
-    HttpServletResponse response ;
 
     public Long getStartTime() {
         return startTime;
@@ -236,9 +224,16 @@ public class JsonResult {
     public void setE(Exception e) {
         this.e = e;
     }
-    
-    
-    
-    
-    
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
 }
